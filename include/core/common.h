2b49b9a0a7b5a49a96d6c3d5631c85a782ee83fe
monellz <rxzhong@outlook.com>
2022-03-26 22:22:27 +0800

    remove intset

--- include/core/common.h
+++ include/core/common.h
@@ -27,14 +27,6 @@ using i32 = int32_t;
 
 #define UNREACHABLE() ERR_EXIT("control flow should never reach here")
 
-template <class... Ts>
-struct overloaded : Ts... {
-  using Ts::operator()...;
-};
-// explicit deduction guide (not needed as of C++20)
-template <class... Ts>
-overloaded(Ts...) -> overloaded<Ts...>;
-
 inline std::vector<std::string_view> split(std::string_view sv,
                                            std::string_view delims) {
   std::vector<std::string_view> output;
@@ -46,67 +38,6 @@ inline std::vector<std::string_view> split(std::string_view sv,
   }
   return output;
 }
-
-// TODO: more efficient way(consume src_set)
-template <typename T>
-inline void union_inplace(std::unordered_set<T>& dst,
-                          const std::unordered_set<T>& src) {
-  for (auto c : src) dst.insert(c);
-}
-
-constexpr size_t FLOOR_LOG2(size_t x) {
-  return x == 1 ? 0 : FLOOR_LOG2(x >> 1) + 1;
-}
-constexpr size_t CEIL_LOG2(size_t x) {
-  return x == 1 ? 0 : FLOOR_LOG2(x - 1) + 1;
-}
-
-constexpr size_t LOG2(size_t x) { return CEIL_LOG2(x); }
-
-template <typename T>
-class IntSet {
- private:
-  T* _data;
-  size_t _size;
-  size_t _capacity;
-
- public:
-  static constexpr size_t _TSIZE = 8 * sizeof(T);
-  static constexpr size_t _BITLOG = LOG2(_TSIZE);
-  static constexpr T _MASK = _TSIZE - 1;
-
-  IntSet(size_t elem_num) {
-    _capacity = (elem_num + _TSIZE - 1) / _TSIZE;
-    _data = new T[_capacity];
-    memset(_data, 0, sizeof(T) * _capacity);
-  }
-
-  bool operator==(const IntSet& other) const {
-    for (size_t i = 0; i < _size; ++i) {
-      if (_data[i] != other._data[i]) return false;
-    }
-    return true;
-  }
-
-  void insert(size_t val) {
-    assert((val >> _BITLOG) < _capacity);
-    _data[val >> _BITLOG] |= (1 << (val & _MASK));
-  }
-
-  void erase(size_t val) {
-    assert((val >> _BITLOG) < _capacity);
-    _data[val >> _BITLOG] &= ~(1 << (val & _MASK));
-  }
-
-  bool check(size_t val) {
-    assert((val >> _BITLOG) < _capacity);
-    return (_data[val >> _BITLOG] & (1 << (val & _MASK))) != 0;
-  }
-
-  void union_inplace(const IntSet& other) {}
-
-  ~IntSet() { delete[] _data; }
-};
 
 }  // namespace parsergen
 
